18de4afef19e9865775df521c39554a1c569425b
dfilatov <dfilatov@inbox.ru>
2010-04-02 23:39:18 +0400

    initial

---
+++ jquery.jemplate.js
@@ -0,0 +1,18 @@
+/**
+ * Simple template plugin
+ *
+ * Copyright (c) 2010 Filatov Dmitry (alpha@zforms.ru)
+ * Dual licensed under the MIT and GPL licenses:
+ * http://www.opensource.org/licenses/mit-license.php
+ * http://www.gnu.org/licenses/gpl.html
+ *
+ * @version 1.0.0
+ */
+
+$.jemplate = function(template, substs) {
+
+	return template.replace(/\{\$([^}]+)\}/g, function(match, name) {
+		return substs[name];
+	});
+
+};
